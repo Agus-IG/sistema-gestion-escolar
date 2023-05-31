21bc9d67559c774a355bdfee123229d4c1a7d9ea
Agus-IG <agusig026@gmail.com>
2023-05-31 16:07:09 -0300

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const router = require('');
+
+class Server{
+    constructor(){
+        this.app = express();
+    }
+}
---
+++ routes/students.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routerStudents = express.Router();
+const studentsControllers = require('../controllers/studentsController');
+
+//* Definicion de rutas y derivacion al controlodaro correspondiente
+
+//Ruta para mostrar todos los estudiantes
+routerStudents.get();
+
+//Ruta para mostrar un estudiante por su id
+routerStudents.get();
+
+//Ruta para crear un estudiante
+routerStudents.post();
+
+//Ruta para actualizar un estudiante por su id
+routerStudents.put();
+
+//Ruta para eliminar un estudiante por su id
+routerStudents.delete();
+
+module.exports = routerStudents;
